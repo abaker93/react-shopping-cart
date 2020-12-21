340ffe66f84805e2399c9ab46b549be10c6beb78
abaker93 <anna.baker.design@gmail.com>
2020-12-21 18:26:00 -0500

    home styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,42 +2,46 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className="footer">
+        <footer>
             <div>
-                <h3>Shout Out</h3>
-                <p>Thanks to the ladies at Brimful for the imagery that appears in this demo. Head on over to their site to shop these products for real.</p>
+                <div className="links">
+                    <div>
+                        <h3>Shout Out</h3>
+                        <p>Thanks to the ladies at Brimful for the imagery that appears in this demo. Head on over to their site to shop these products for real.</p>
+                    </div>
+                    <div>
+                        <h3>Information</h3>
+                        <ul>
+                            <li>FAQ</li>
+                            <li>Shipping & Returns</li>
+                            <li>Terms & Conditions</li>
+                            <li>Impact Program</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h3>Navigate</h3>
+                        <ul>
+                            <li>Home</li>
+                            <li>Shop</li>
+                            <li>About</li>
+                            <li>Contact</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h3>Follow Us</h3>
+                        <ul>
+                            <li>Facebook</li>
+                            <li>Twitter</li>
+                            <li>Pinterest</li>
+                            <li>Instagram</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="copyright">
+                    <p>&copy; 2020 <a href="http://annabaker.design" target="_blank" rel="noreferrer">Anna Baker Design</a></p>
+                </div>
             </div>
-            <div>
-                <h3>Information</h3>
-                <ul>
-                    <li>FAQ</li>
-                    <li>Shipping & Returns</li>
-                    <li>Terms & Conditions</li>
-                    <li>Impact Program</li>
-                </ul>
-            </div>
-            <div>
-                <h3>Navigate</h3>
-                <ul>
-                    <li>Home</li>
-                    <li>Shop</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
-            </div>
-            <div>
-                <h3>Follow Us</h3>
-                <ul>
-                    <li>Facebook</li>
-                    <li>Twitter</li>
-                    <li>Pinterest</li>
-                    <li>Instagram</li>
-                </ul>
-            </div>
-            <div className="copyright">
-                <p>&copy; 2020 <a href="http://annabaker.design" target="_blank" rel="noreferrer">Anna Baker Design</a></p>
-            </div>
-        </div>
+        </footer>
     )
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -43,7 +43,7 @@ const Home = () => {
                 <div>
                     <h1>Whimsical Toys</h1>
                     <p>Our carefully curated collections of handcrafted toys spark joy and imagination. Perfect for creative kiddos.</p>
-                    <button type="button">Shop Now</button>
+                    <a href="./shop"><button type="button">Shop Now</button></a>
                 </div>
                 
             </div>
@@ -51,30 +51,36 @@ const Home = () => {
                 <div>
                     <h2>Shop, Play, Love</h2>
                     <p>This online boutique was created from our passion for unique toys, decor, and books for babies. We strive to provide the most timeless well-crafted items that we believe will enrich your parenting experience and bring endless joy to your little ones.</p>
-                    <button type="button">About</button>
+                    <a href="./about"><button type="button">About</button></a>
                 </div>
             </div>
             <div className="shop">
-                <span className="subheader">New at Octoon</span>
-                <h2>The Bright & Bold Collection</h2>
-                <button type="button">Shop Now</button>
+                <div>
+                    <span className="subheader">New at Octoon</span>
+                    <h2>The Bright & Bold Collection</h2>
+                    <a href="./shop"><button type="button">Shop Now</button></a>
+                </div>
                 {products.map(e => {
                     return (
                         <div className="card">
                             <img src={e.img} alt={e.title} />
-                            <p>{e.title}</p>
-                            <p>${e.price.toFixed(2)}</p>
+                            <div>
+                                <h3>{e.title}</h3>
+                                <p>${e.price.toFixed(2)}</p>
+                            </div>
                         </div>
                     )
                 })}
             </div>
             <div className="newsletter">
-                <h2>Newsletter</h2>
-                <p>Sign up to get 15% off your first order!</p>
-                <form>
-                    <input type="email" placeholder="Email address" />
-                    <button type="button">Sign Up</button>
-                </form>
+                <div>
+                    <h2>Newsletter</h2>
+                    <p>Sign up to get 15% off your first order!</p>
+                    <form>
+                        <input type="email" placeholder="Email address" />
+                        <button type="button">Sign Up</button>
+                    </form>
+                </div>
             </div>
             <Footer />
         </div>
